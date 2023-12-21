0db8368e80254d19af44c4847f21bedcd4904a9f
admin <testcsea@gmail.com>
2023-12-21 12:46:26 +0530

    modified app.java and added add.java

--- maven/src/main/java/aaa/App.java
+++ maven/src/main/java/aaa/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+	int a=0,b=0,c=0;
+	a=add.addition(2,3);
+	System.out.println("add="+a);
     }
 }
---
+++ maven/src/main/java/aaa/add.java
@@ -0,0 +1,7 @@
+package aaa;
+public class add{
+	public static int addition(int a,int b){
+		return a+b;
+	}
+}
+
